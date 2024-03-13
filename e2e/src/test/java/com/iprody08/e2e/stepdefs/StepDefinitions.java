a082fb2b126e1a6e3c7963afab3e33b7c3746639
Kirill Kozlov <space_cell@proton.me>
2024-03-13 22:58:31 +0500

    Add cucumber for customer-service

---
+++ e2e/src/test/java/com/iprody08/e2e/stepdefs/StepDefinitions.java
@@ -0,0 +1,22 @@
+package com.iprody08.e2e.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("I have completed course in my customer")
+    public void i_have_completed_course_in_my_customer() {
+        System.out.println("Completed course.");
+    }
+
+    @When("I click the send feedback button")
+    public void i_click_the_send_feedback_button() {
+        System.out.println("Button");
+    }
+
+    @Then("I should post a review")
+    public void i_should_post_a_review() {
+        System.out.println("Completed post");
+    }
+}
