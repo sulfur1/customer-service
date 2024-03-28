1287960d2be6ad4c429ce2e95caf18f136ed05c2
Sakhmina Asadullaeva <asakhmina@gmail.com>
2024-03-28 19:08:19 +0300

    #24 added repository test findAll

--- customer-service-app/src/main/java/com/iprody08/customerservice/repositories/CountryRepository.java
+++ customer-service-app/src/main/java/com/iprody08/customerservice/repositories/CountryRepository.java
@@ -1,7 +1,13 @@
 package com.iprody08.customerservice.repositories;
 
 import com.iprody08.customerservice.entities.Country;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+@Transactional(readOnly = true)
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+
 }
