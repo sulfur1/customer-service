d1facb36e2b083cc31ced8187d6f85c1c0ea58e5
AlexandrLezh <sasha_l@inbox.lv>
2024-04-09 18:51:04 +0300

    ContactDetailsServiceImpl tests #27

--- customer-service-app/src/test/java/com/iprody08/customerservice/for_tests/CustomerServiceConstants.java
+++ customer-service-app/src/test/java/com/iprody08/customerservice/for_tests/CustomerServiceConstants.java
@@ -1,9 +1,20 @@
 package com.iprody08.customerservice.for_tests;
 
+import com.iprody08.customerservice.dto.ContactDetailsDto;
+import com.iprody08.customerservice.entities.ContactDetails;
 import com.iprody08.customerservice.entities.Country;
+
 import java.time.Instant;
 
 public final class CustomerServiceConstants {
+
+    public static final String EMAIL_FIRST = "firstEmail@gmail.com";
+
+    public static final String EMAIL_SECOND = "secondEmail@gmail.com";
+
+    public static final String TELEGRAM_ID_FIRST = "@firstTelegramId";
+
+    public static final String TELEGRAM_ID_SECOND = "@secondTelegramId";
 
     public static final Country US = new Country(
             1L,
@@ -19,6 +30,42 @@ public final class CustomerServiceConstants {
             "LVA",
             "Latvia",
             null,
+            Instant.now(),
+            Instant.now()
+    );
+
+    public static final ContactDetails CONTACT_DETAILS_FIRST = new ContactDetails(
+            1L,
+            null,
+            EMAIL_FIRST,
+            TELEGRAM_ID_FIRST,
+            Instant.now(),
+            Instant.now()
+    );
+
+    public static final ContactDetails CONTACT_DETAILS_SECOND = new ContactDetails(
+            2L,
+            null,
+            EMAIL_SECOND,
+            TELEGRAM_ID_SECOND,
+            Instant.now(),
+            Instant.now()
+    );
+
+    public static final ContactDetailsDto FIRST_CONTACT_DETAILS_DTO = new ContactDetailsDto(
+            1L,
+            1L,
+            EMAIL_FIRST,
+            TELEGRAM_ID_FIRST,
+            Instant.now(),
+            Instant.now()
+    );
+
+    public static final ContactDetailsDto SECOND_CONTACT_DETAILS_DTO = new ContactDetailsDto(
+            2L,
+            2L,
+            EMAIL_SECOND,
+            TELEGRAM_ID_SECOND,
             Instant.now(),
             Instant.now()
     );
