c3ca5ac623b293dfc37e8f14b1bed8844f269491
AlexandrLezh <sasha_l@inbox.lv>
2024-04-09 16:33:41 +0300

    #27 CountryMapper moved to @Spy

--- customer-service-app/src/test/java/com/iprody08/customerservice/services/impl/CountryServiceImplTest.java
+++ customer-service-app/src/test/java/com/iprody08/customerservice/services/impl/CountryServiceImplTest.java
@@ -4,58 +4,58 @@ import com.iprody08.customerservice.dto.CountryDTO;
 import com.iprody08.customerservice.dto.mapper.CountryMapper;
 import com.iprody08.customerservice.entities.Country;
 import com.iprody08.customerservice.repositories.CountryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CountryServiceImplTest {
     private static final long ID = 1L;
+    private Country country = new Country();
 
     @Mock
     private CountryRepository countryRepository;
-
-    @Mock
-    private CountryMapper countryMapper;
+    @Spy
+    private CountryMapper countryMapper = Mappers.getMapper(CountryMapper.class);
 
     @InjectMocks
     private CountryServiceImpl service;
 
-    @Test
-    void findEventShouldCallRepository() {
-
-        // given
-        Country country = mock(Country.class);
-        CountryDTO expected = mock(CountryDTO.class);
-        when(countryRepository.findById(ID)).thenReturn(Optional.of(country));
-        when(countryMapper.countryToDTO(country)).thenReturn(expected);
-
-        // when
-        Optional<CountryDTO> countryDTOOptional = service.findCountryById(ID);
-        var result = countryDTOOptional.get();
-
-        // then
-        assertEquals(expected, result);
-        verify(countryRepository, times(1)).findById(ID);
-        verify(countryMapper, times(1)).countryToDTO(country);
+    @BeforeEach
+    void setUp() {
+        country.setId(ID);
+        country.setCountryCode("US");
+        country.setName("United States");
     }
 
+        @Test
+        void testCountryServiceImplFindCountryByIdReturnCountryDto() {
+            // given
+            when(countryRepository.findById(ID)).thenReturn(Optional.of(country));
+
+            // when
+            Optional<CountryDTO> result = service.findCountryById(ID);
+
+            // then
+            assertTrue(result.isPresent(), "CountryDto should be present");
+            assertEquals(country.getCountryCode(), result.get().getCountryCode());
+            assertEquals(country.getName(), result.get().getName());
+        }
+
     @Test
-    public void testFindCountryByIdWhenIdDoesNotExist() {
+    public void testCountryServiceImplFindCountryByIdWhenIdDoesNotExist() {
         // given
         when(countryRepository.findById(ID)).thenReturn(Optional.empty());
 
@@ -64,28 +64,23 @@ class CountryServiceImplTest {
 
         // then
         assertEquals(Optional.empty(), result);
-        verify(countryRepository, times(1)).findById(ID);
-        verifyNoInteractions(countryMapper);
     }
 
     @Test
-    public void testFindAll() {
+    public void testCountryServiceImplFindAll() {
         // given
-        List<Country> countries = new ArrayList<>();
-        countries.add(new Country());
-        countries.add(new Country());
+        List<Country> countries = List.of(
+                new Country(),
+                new Country());
         when(countryRepository.findAll()).thenReturn(countries);
-        List<CountryDTO> expectedDTOs = new ArrayList<>();
-        expectedDTOs.add(new CountryDTO());
-        expectedDTOs.add(new CountryDTO());
-        when(countryMapper.countryToDTO(any())).thenReturn(new CountryDTO());
+        List<CountryDTO> expectedDTOs = List.of(
+                new CountryDTO(),
+                new CountryDTO());
 
         // when
         List<CountryDTO> result = service.findAll(null);
 
         // then
         assertEquals(expectedDTOs.size(), result.size());
-        verify(countryRepository, times(1)).findAll();
-        verify(countryMapper, times(countries.size())).countryToDTO(any());
     }
 }
