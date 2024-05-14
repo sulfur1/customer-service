4f134fb22d87003ec54ce81915f033dd3102097d
Sakhmina Asadullaeva <asakhmina@gmail.com>
2024-05-14 19:56:11 +0300

    fixed validation of CustomerDto by adding spring-boot-validation-starter #59
    moved up spring-boot-starter-validation dependency in app module #59

--- customer-service-app/src/main/java/com/iprody08/customerservice/controllers/CustomerController.java
+++ customer-service-app/src/main/java/com/iprody08/customerservice/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import com.iprody08.customerservice.services.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -21,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
