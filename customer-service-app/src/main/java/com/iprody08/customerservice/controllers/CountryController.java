cf7c7d8cfc0760719961867acc99d2a078ec09fd
Alexey <sulfur1917@gmail.com>
2024-05-17 10:19:35 +0300

    add endpoint get all countries

---
+++ customer-service-app/src/main/java/com/iprody08/customerservice/controllers/CountryController.java
@@ -0,0 +1,43 @@
+package com.iprody08.customerservice.controllers;
+
+import com.iprody08.customerservice.dto.CountryDto;
+import com.iprody08.customerservice.services.CountryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/countries", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @Operation(summary = "Get all countries", description = "Returns all countries")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Countries found"),
+    })
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CountryDto> getAllCountries(@PageableDefault(size = 248) Pageable pageable) {
+        log.info("Get all countries");
+        return countryService.findAll(pageable);
+    }
+}
---
+++ customer-service-app/src/test/java/com/iprody08/customerservice/controllers/CountryControllerTest.java
@@ -0,0 +1,57 @@
+package com.iprody08.customerservice.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iprody08.customerservice.dto.CountryDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CountryControllerTest {
+
+    private static final String BASE_URL = "/api/countries";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllCountries() throws Exception {
+        //given
+
+        int sizeCountryList = 248;
+
+        //when
+
+        MvcResult result = mvc.perform(get(BASE_URL))
+
+        //then
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        List<CountryDto> countryDtos = objectMapper
+                .readValue(response, new TypeReference<List<CountryDto>>() {
+        });
+
+        assertThat(countryDtos).isNotEmpty();
+        assertThat(countryDtos).hasSize(sizeCountryList);
+    }
+}
